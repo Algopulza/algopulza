8e2ba6d4b1ddfae449c252da2dd627e9384756d0
hengzizng <hengzizng@naver.com>
2022-04-19 13:39:12 +0900

    Feat: ResponseMessage 작성

---
+++ backend/src/main/java/com/algopulza/backend/common/model/ResponseMessage.java
@@ -0,0 +1,17 @@
+package com.algopulza.backend.common.model;
+
+public class ResponseMessage {
+
+    private ResponseMessage() {
+    }
+
+    // Login
+    public static final String LOGIN = "로그인 성공";
+    public static final String LOGOUT = "로그아웃 성공";
+    public static final String GET_USER_INFO = "유저 정보 조회 성공";
+    public static final String MODIFY_USER_INFO = "유저 정보 수정 성공";
+
+    // Problem
+    public static final String GET_PROBLEM_LIST = "문제 목록 조회 성공";
+
+}
