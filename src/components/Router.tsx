83cd2d7b7be3706d49be9e45ca17a7ec9194cf6b
runkey <csi9509@naver.com>
2022-04-19 10:16:33 +0900

    S06P31A408-37 Feat: URL 연결

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,13 +4,26 @@ import {
     Routes,
     Navigate,
   } from "react-router-dom";
+  import Home from "routes/Home";
 import Landing from "routes/Landing";
+import Recommand from "routes/Recommand";
+import Random from "routes/Random";
+import Category from "routes/Category";
+import Analysis from "routes/Analysis";
+import Search from "routes/Search";
 
 const AppRouter = () => {
     return(
         <Router>
             <Routes>
-                <Route path="/" element={<Landing />} />
+                <Route path="" element={<Landing />} />
+                <Route path="/" element={<Home />}>
+                <Route path="/recommand" element={<Recommand />} />
+                <Route path="/random" element={<Random />} />
+                <Route path="/category" element={<Category />} />
+                <Route path="/analysis" element={<Analysis />} />
+                <Route path="/search" element={<Search />} />
+                </Route>
             </Routes>
         </Router>
 
