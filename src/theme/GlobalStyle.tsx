32c6d52030b79053a453b2baba476716ee8be9d2
runkey <csi9509@naver.com>
2022-04-19 10:15:55 +0900

    S06P31A408-37 Feat: 글로벌 폰트 추가

---
+++ src/theme/GlobalStyle.tsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+@font-face {
+    font-family: 'GmarketSansBold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+body {
+    font-family: GmarketSansMedium;
+}
+`;
+
+export default GlobalStyle;
