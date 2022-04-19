bb1e9886cf2ec4329e27ecaa3a1a6718ead87c45
이민정 <3sally@naver.com>
2022-04-19 02:08:30 +0000

    Revert "Merge branch 'feature/S06P31A408-117' into 'develop'"
    This reverts merge request !4

--- backend/src/main/java/com/algopulza/backend/db/entity/Member.java
+++ backend/src/main/java/com/algopulza/backend/db/entity/Member.java
@@ -1,35 +1,4 @@
 package com.algopulza.backend.db.entity;
 
-import lombok.Getter;
-
-import javax.persistence.*;
-
-@Getter
-@Entity
-@Table(name = "member")
-public class Member extends BaseTimeEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "tier")
-    private Tier tier;
-
-    @Column
-    private String name;
-
-    @Column
-    private String profileImage;
-
-    @Column
-    private int solveCount;
-
-    @Column
-    private String email;
-
-    @Column
-    private int daysCount;
-
+public class Member {
 }
