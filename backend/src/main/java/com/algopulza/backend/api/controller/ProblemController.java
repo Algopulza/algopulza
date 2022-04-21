db6f05e31837b77870eab71fb009c2454364c1d8
hengzizng <hengzizng@naver.com>
2022-04-21 13:11:39 +0900

    S06P31A408-120 Refactor: ProblemController 문제 수집 API swagger 설명 수정, 반환 메시지 수정

--- backend/src/main/java/com/algopulza/backend/api/controller/ProblemController.java
+++ backend/src/main/java/com/algopulza/backend/api/controller/ProblemController.java
@@ -1,41 +1,43 @@
 package com.algopulza.backend.api.controller;
 
-import com.algopulza.backend.common.exception.handler.ErrorResponse;
 import com.algopulza.backend.api.service.ProblemService;
+import com.algopulza.backend.common.exception.handler.ErrorResponse;
 import com.algopulza.backend.common.model.BaseResponseBody;
+import com.algopulza.backend.common.model.ResponseMessage;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.RequiredArgsConstructor;
-
 import io.swagger.annotations.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.algopulza.backend.common.model.ResponseMessage.*;
 
 
-@RequiredArgsConstructor
-@CrossOrigin(origins = {"*"}, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
-        RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.OPTIONS})
-@Api(value = "문제 추천 API", tags = { "problem" })
-@RequestMapping("/api/v1/problem")
+@CrossOrigin(origins = {"*"}, methods = {RequestMethod.GET,
+        RequestMethod.POST,
+        RequestMethod.PUT,
+        RequestMethod.PATCH,
+        RequestMethod.DELETE,
+        RequestMethod.OPTIONS})
+@Api(value = "문제관리 API", tags = {"problem"})
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/problems")
 public class ProblemController {
 
     private final ProblemService problemService;
 
-    @ApiOperation(value = "문제 정보 수집", notes = "리뷰를 수정합니다.")
-    @ApiResponses({@ApiResponse(code = 201, message = PUT_PROBLEMS),
-            @ApiResponse(code = 400, message = "Invalid Input 오류", response = ErrorResponse.class),
-            @ApiResponse(code = 401, message = "권한 인증 오류", response = ErrorResponse.class),
-            @ApiResponse(code = 404, message = "Not Found 오류", response = ErrorResponse.class),
-            @ApiResponse(code = 500, message = "서버 에러", response = ErrorResponse.class)})
-    @PutMapping()
+    @PutMapping("")
+    @ApiOperation(value = "문제 정보 수집하기", notes = "Solved.ac API로 문제 정보를 수집하는 API 입니다.")
+    @ApiResponses({@ApiResponse(code = 200, message = ResponseMessage.PUT_PROBLEM_LIST_SUCCESS, response = ErrorResponse.class),
+            @ApiResponse(code = 400, message = ResponseMessage.BAD_REQUEST, response = ErrorResponse.class),
+            @ApiResponse(code = 401, message = ResponseMessage.UNAUTHORIZED, response = ErrorResponse.class),
+            @ApiResponse(code = 403, message = ResponseMessage.ACCESS_DENIED, response = ErrorResponse.class),
+            @ApiResponse(code = 404, message = ResponseMessage.NOT_FOUND, response = ErrorResponse.class)})
     public ResponseEntity<BaseResponseBody> addProblems() throws JsonProcessingException {
 
 
-        return ResponseEntity.ok(BaseResponseBody.of(
-                HttpStatus.OK, PUT_PROBLEMS, problemService.addProblems(0, 0)));
+        return ResponseEntity.ok(BaseResponseBody.of(HttpStatus.OK, ResponseMessage.PUT_PROBLEM_LIST_SUCCESS, problemService.addProblems(0, 0)));
     }
 
 
