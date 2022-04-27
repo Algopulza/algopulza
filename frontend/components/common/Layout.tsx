6239a052133bc0c91fd3bb91c498436f522c0a16
2dend <2dend0713@gmail.com>
2022-04-28 02:39:56 +0900

    S06P31A408-187 Feat: Layout 테스트

--- frontend/components/common/Layout.tsx
+++ frontend/components/common/Layout.tsx
@@ -1,8 +1,9 @@
+import { ReactElement } from 'react'
 import NavBar from './NavBar'
 import Footer from './Footer'
 
 type Props = {
-  children: JSX.Element
+  children: ReactElement
 }
 
 export default function Layout({ children }: Props) {
