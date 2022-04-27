37c3784b6db3fce3f5eafa1a34e24182e5a99c03
2dend <2dend0713@gmail.com>
2022-04-27 22:43:06 +0900

    Docs: Next.js TIL 문서 생성

--- backend/src/main/java/com/algopulza/backend/db/repository/MemberHasOrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.algopulza.backend.db.repository;
-
-import com.algopulza.backend.db.entity.MemberHasOrganization;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberHasOrganizationRepository extends JpaRepository<MemberHasOrganization, Long> {
-}
