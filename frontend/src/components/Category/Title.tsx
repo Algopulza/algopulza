912496c454fcd7dc8b3f71036f914b1825d47827
runkey <csi9509@naver.com>
2022-04-27 16:38:43 +0900

    S06P31A408-137 Feat: 카테고리 탭에 검색창 추가

--- frontend/src/components/Category/Title.tsx
+++ frontend/src/components/Category/Title.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import SearchInput from "components/Search/SearchInput";
 
 const Container = styled.section`
   margin-bottom: 50px;
@@ -11,6 +12,7 @@ const Title = () => {
   return (
     <Container>
       전체 문제 목록
+      <SearchInput/>
     </Container>
   )
 }
