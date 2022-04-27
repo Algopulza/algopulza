2eaa60b8fd3dffb1ed5f3da774a3662d678beaf9
2dend <2dend0713@gmail.com>
2022-04-28 01:31:34 +0900

    S06P31A408-187 Feat: Page 간 Router

---
+++ frontend/components/recommendation/Subject.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.section`
+  background: #90caf9;
+`
+
+function Subject() {
+  return (
+    <Container>
+      <p>These are problems regarding a subject</p>
+    </Container>
+  )
+}
+
+export default Subject
