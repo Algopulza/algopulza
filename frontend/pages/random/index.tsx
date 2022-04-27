46e21f34b98a7732e9fffc1277b2bc920d15770e
2dend <2dend0713@gmail.com>
2022-04-28 02:55:34 +0900

    S06P31A408-187 Feat: Layout(NavBar, Footer) 적용

--- frontend/pages/random/index.tsx
+++ frontend/pages/random/index.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import { ReactElement } from 'react'
+import Layout from '../../components/common/Layout'
 import styled from 'styled-components'
 
 const Container = styled.section`
@@ -9,7 +11,7 @@ const Title = styled.div`
   margin: 0;
 `
 
-function Random() {
+export default function Random() {
   return (
     <Container>
       <Title>This is a Random Page.</Title>
@@ -21,4 +23,8 @@ function Random() {
   )
 }
 
-export default Random
+Random.getLayout = function getLayout(random: ReactElement) {
+  return (
+    <Layout>{random}</Layout>
+  )
+}
