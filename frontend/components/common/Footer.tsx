de967342a87876068f72d5eabef45d32acc9bf47
2dend <2dend0713@gmail.com>
2022-04-28 02:01:38 +0900

    S06P31A408-187 Feat: common component(NavBar, Footer) 구조화

---
+++ frontend/components/common/Footer.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.section`
+  background: #90caf9;
+`
+
+function Footer() {
+  return (
+    <Container>
+      <p>These are problems regarding a subject</p>
+    </Container>
+  )
+}
+
+export default Footer
