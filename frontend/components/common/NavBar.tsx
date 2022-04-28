0bd79cec9698db2d592a4d05f44ae2732b927cac
2dend <2dend0713@gmail.com>
2022-04-29 00:13:30 +0900

    S06P31A408-82 Feat: Navbar 선택 링크 색 전환 적용

--- frontend/components/common/NavBar.tsx
+++ frontend/components/common/NavBar.tsx
@@ -7,8 +7,8 @@ const Container = styled.section`
   display: grid;
   grid-template-columns: 30vw 40vw 30vw;
   align-items: center;
-  height: 60px;
-  background: #1565C0;
+  height: 7vh;
+  background: #E3F2FD;
 `
 
 const Pages = styled.div`
@@ -23,18 +23,20 @@ export type Route = {
 }
 
 export default function NavBar() {
+  const [isSelected, setIsSelected] = useState('/recommendation')
   const [routes, setRoutes] = useState<Route[]>([
     { name: '추천', path: '/recommendation' },
     { name: '랜덤', path: '/random' },
     { name: '검색', path: '/list' },
     { name: '분석', path: '/analysis' }
   ])
+  const onClick = (path: string) => setIsSelected(path)
 
   return (
     <Container>
       <ServiceName />
       <Pages>
-        {routes.map(route => <NavItem key={route.name} route={route} />)}
+        {routes.map(route => <NavItem key={route.name} route={route} isSelected={isSelected} onClick={onClick} />)}
       </Pages>
       <div />
     </Container>
--- frontend/components/common/navbar/NavItem.tsx
+++ frontend/components/common/navbar/NavItem.tsx
@@ -2,18 +2,33 @@ import { Route } from '../NavBar'
 import Link from 'next/link'
 import styled from 'styled-components'
 
-const Container = styled.section`
-  
+const Text = styled.span<{ cond: boolean }>`
+  color: ${(props) => (props.cond ? "#FFC94D" : "#000000")};
+
+  &:hover {
+    color: #FFC94D;
+  }
 `
 
 type RouteProps = {
-  route: Route
+  route: Route,
+  isSelected: String,
+  onClick(path: string): void
 }
 
-export default function NavItem({ route }: RouteProps) {
+export default function NavItem({ route, isSelected, onClick }: RouteProps) {
   const { name, path } = route
 
   return (
-    <Link href={path}><a>{name}</a></Link>
+    <Link href={path}>
+      <a>
+        <Text
+          onClick={() => onClick(path)}
+          cond={isSelected == path ? true : false}
+        >
+          {name}
+        </Text>
+      </a>
+    </Link>
   )
 }
