dee5822da3df2227b86b23508ae5ad18fa8b3ad4
2dend <2dend0713@gmail.com>
2022-04-28 15:52:40 +0900

    S06P31A408-149 Feat: Landing Page Description Service Description

--- frontend/components/landing/description/ServiceDescription.tsx
+++ frontend/components/landing/description/ServiceDescription.tsx
@@ -1,13 +1,23 @@
 import styled from 'styled-components'
 
 const Container = styled.section`
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Title = styled.p`
+  margin: 0;
+  line-height: 2;
+  color: #999999;
 `
 
 export default function ServiceDescription() {
   return (
     <Container>
-      <p>This is a description area.</p>
+      <Title>알고리즘 문제풀이 능력은 필수</Title>
+      <Title>막 풀지 말고 알고 푸세요</Title>
+      <Title>저희가 알려드릴게요</Title>
     </Container>
   )
 }
