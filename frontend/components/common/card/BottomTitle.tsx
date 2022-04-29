c0e4d81a95a73cde8671de14720e530799ea6085
2dend <2dend0713@gmail.com>
2022-04-29 14:07:41 +0900

    S06P31A408-147 Feat: Recommendation Page 이전

---
+++ frontend/components/common/card/BottomTitle.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Container = styled.section`
+  text-align: center;
+  font-size: 1.2vw;
+  font-weight: 700;
+`
+
+type TitleProps = {
+  title: string,
+}
+
+export default function BottomTitle({ title }: TitleProps) {
+  return (
+    <Container>{title}</Container>
+  )
+}
