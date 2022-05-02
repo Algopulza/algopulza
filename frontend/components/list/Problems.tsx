1e50533c2683c6d1e2f6915de65fa8efd64d1006
2dend <2dend0713@gmail.com>
2022-05-03 01:27:26 +0900

    S06P31A408-200 Feat: List page 초기 세팅

---
+++ frontend/components/list/Problems.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Container = styled.section`
+  background: #FFC94D;
+`
+
+export default function Problems() {
+  return (
+    <Container>
+      hi
+    </Container>
+  )
+}
