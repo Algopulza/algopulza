890c75a490ff6335206b7a320e91075b298846a1
2dend <2dend0713@gmail.com>
2022-05-03 21:37:53 +0900

    Feat: ButtonLogin, ButtonExtension components 구조화 및 routing, redirecting 설정

--- frontend/components/landing/brand/button/ButtonExtension.tsx
+++ frontend/components/landing/brand/button/ButtonExtension.tsx
@@ -1,16 +1,24 @@
-import Link from 'next/link'
 import styled from 'styled-components'
 
 const Text = styled.div`
   text-align: center;
   font-size: 1.3vw;
-  color: #999999;
+  color: #616161;
+`
+
+const Words = styled.span`
+  color: #FFC94D;
+  cursor: pointer;
 `
 
 export default function ButtonExtension() {
+  const handleClick = () => {
+    window.open("https://chrome.google.com/webstore/category/extensions?hl=ko")
+  }
+
   return (
     <Text>
-      익스텐션 <Link href="/recommendation"><a>설치</a></Link>하기
+      익스텐션을 <Words onClick={handleClick}>설치</Words>해 보세요
     </Text>
   )
 }
--- frontend/components/landing/brand/service/ServiceName.tsx
+++ frontend/components/landing/brand/service/ServiceName.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Container = styled.section`
   display: flex;
   flex-direction: column;
-  margin-bottom: 80px;
+  margin-bottom: 70px;
 `
 
 const Title = styled.p`
