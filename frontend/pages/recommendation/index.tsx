d8173c4f07cedc21bf84d7fdff7ea2ad3be3c347
2dend <2dend0713@gmail.com>
2022-05-03 23:38:56 +0900

    Feat: Recommendation component 구조화

--- frontend/pages/recommendation/index.tsx
+++ frontend/pages/recommendation/index.tsx
@@ -8,12 +8,11 @@ const Container = styled.section`
   padding: 0vw 3vw;
 `
 
-export type Title = { name: string }
+export type Title = { title: string }
 const titles = [
-  { name: '취약한 태그에 속하는 문제들을 추천해 드려요!' },
-  { name: '최근 자주 풀었던 태그에 속하는 문제들을 추천해 드려요!' },
-  { name: '코딩 테스트에 자주 출제되는 태그에 속하는 문제들을 추천해 드려요!' },
-  { name: '비슷한 실력의 다른 유저들이 많이 푼 문제들을 추천해 드려요!' },
+  { title: '취약한 태그에 속하는 문제들을 추천해 드려요!' },
+  { title: '최근 자주 풀었던 태그들에 속하는 문제들을 추천해 드려요!' },
+  { title: '비슷한 실력의 다른 유저들이 많이 푼 문제들을 추천해 드려요!' },
 ]
 
 export default function Recommendation() {
@@ -22,7 +21,7 @@ export default function Recommendation() {
       <Carousel />
 
       <Container>
-        {titles.map(title => <Subject key={title.name} title={title} />)}
+        {titles.map(title => <Subject key={title.title} title={title} />)}
       </Container>
     </>
   )
