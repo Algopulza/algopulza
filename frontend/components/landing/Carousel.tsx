f31ff7bbb9fda63245e62de3e1a76d207fd1bbd0
2dend <2dend0713@gmail.com>
2022-05-04 23:29:21 +0900

    Feat: Carousel component, slide 추가

--- frontend/components/landing/Carousel.tsx
+++ frontend/components/landing/Carousel.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import Slider from 'react-slick'
-import Slide001 from '../../public/carousel/landing/slide_001.jpg'
-import Slide002 from '../../public/carousel/landing/slide_002.jpg'
-import Slide003 from '../../public/carousel/landing/slide_003.jpg'
+import Slide001 from '../../public/landing/sample_slide_001.jpg'
+import Slide002 from '../../public/landing/sample_slide_002.jpg'
+import Slide003 from '../../public/landing/sample_slide_003.jpg'
+import Slide004 from '../../public/landing/sample_slide_004.jpg'
 import styled from 'styled-components'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
@@ -31,6 +32,7 @@ export default function Carousel() {
         <Image src={Slide001} layout="responsive" alt="landing page carousel slide 001" />
         <Image src={Slide002} layout="responsive" alt="landing page carousel slide 002" />
         <Image src={Slide003} layout="responsive" alt="landing page carousel slide 003" />
+        <Image src={Slide004} layout="responsive" alt="landing page carousel slide 004" />
       </Slider>
     </Container>
   )
