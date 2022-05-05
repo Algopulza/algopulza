5b5b91bf2953e2b3efc06d7db4bdbbac6a5690a0
2dend <2dend0713@gmail.com>
2022-05-05 23:19:32 +0900

    Refactor: NavBar 및 하위 components, 코드 수정

--- frontend/api/api.tsx
+++ frontend/api/api.tsx
@@ -1,34 +1,34 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "http://k6a4081.p.ssafy.io:5000/",
-  headers: {
-    "Content-Type": `application/json`,
-    Authorization: `Bearer ${
-      sessionStorage.getItem("access-token") ||
-      localStorage.getItem("access-token")
-    }`,
-  },
-});
+// export const api = axios.create({
+//   baseURL: "http://k6a4081.p.ssafy.io:5000/",
+//   headers: {
+//     "Content-Type": `application/json`,
+//     Authorization: `Bearer ${
+//       sessionStorage.getItem("access-token") ||
+//       localStorage.getItem("access-token")
+//     }`,
+//   },
+// });
 
-export const backapi = axios.create({
-  baseURL: "http://k6a408.p.ssafy.io:8081/",
-  headers: {
-    "Content-Type": `application/json`,
-    Authorization: `Bearer ${
-      sessionStorage.getItem("access-token") ||
-      localStorage.getItem("access-token")
-    }`,
-  },
-});
+// export const backapi = axios.create({
+//   baseURL: "http://k6a408.p.ssafy.io:8081/",
+//   headers: {
+//     "Content-Type": `application/json`,
+//     Authorization: `Bearer ${
+//       sessionStorage.getItem("access-token") ||
+//       localStorage.getItem("access-token")
+//     }`,
+//   },
+// });
 
-export const fileApi = axios.create({
-  baseURL: "http://k6a408.p.ssafy.io:8081/",
-  headers: {
-    "Content-Type": `multipart/form-data`,
-    Authorization: `Bearer ${
-      sessionStorage.getItem("access-token") ||
-      localStorage.getItem("access-token")
-    }`,
-  },
-});
+// export const fileApi = axios.create({
+//   baseURL: "http://k6a408.p.ssafy.io:8081/",
+//   headers: {
+//     "Content-Type": `multipart/form-data`,
+//     Authorization: `Bearer ${
+//       sessionStorage.getItem("access-token") ||
+//       localStorage.getItem("access-token")
+//     }`,
+//   },
+// });
