533d5951828cd9bd3211eeb4cd336b1e5f72b377
2dend <2dend0713@gmail.com>
2022-05-06 01:35:45 +0900

    Feat: resutl component 작성

--- frontend/components/common/button/ButtonSearching.tsx
+++ frontend/components/common/button/ButtonSearching.tsx
@@ -6,7 +6,7 @@ const Button = styled.button`
   background: #FFC94D;
   border: none;
   border-radius: 15px;
-  box-shadow: 0px 4px px 6px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px 2px rgba(0, 0, 0, 0.25);
   font-size: 1.3vw;
   font-weight: 700;
   color: white;
--- frontend/components/search/Result.tsx
+++ frontend/components/search/Result.tsx
@@ -1,17 +1,62 @@
-import Header from './result/Header'
-import Row from './result/Row'
-import styled from 'styled-components'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
-const Container = styled.section`
-  height: 65vh;
-  background: #FFC94D;
-`
+function createData(
+  id: number,
+  problem: string,
+  tier: string,
+  level: number,
+  tag: string
+) {
+  return { id, problem, tier, level, tag }
+}
+
+const rows = [
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현'),
+  createData(14403, '로봇 청소기', '골드', 5, '구현')
+]
 
 export default function Result() {
   return (
-    <Container>
-      <Header />
-      <Row />
-    </Container>
-  )
+    <TableContainer component={Paper}>
+      <Table aria-label="search">
+        <TableHead sx={{ background: '#FFC94D' }}>
+          <TableRow>
+            <TableCell align="center">ID</TableCell>
+            <TableCell align="center">Problem</TableCell>
+            <TableCell align="center">Tier</TableCell>
+            <TableCell align="center">Level</TableCell>
+            <TableCell align="center">Tag</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center" component="th" scope="row">{row.id}</TableCell>
+              <TableCell align="center">{row.problem}</TableCell>
+              <TableCell align="center">{row.tier}</TableCell>
+              <TableCell align="center">{row.level}</TableCell>
+              <TableCell align="center">{row.tag}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
