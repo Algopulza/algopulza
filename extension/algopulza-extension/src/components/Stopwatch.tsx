93b27dca342d97ba85816769e702ea27c7a78185
2dend <2dend0713@gmail.com>
2022-05-09 14:02:04 +0900

    Design: extension, stopwatch 스타일 조정

--- extension/algopulza-extension/src/components/Stopwatch.tsx
+++ extension/algopulza-extension/src/components/Stopwatch.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import '../styles/Stopwatch.css'
 
 export default function Stopwatch() {
   const [time, setTime] = useState(0)
@@ -23,12 +24,12 @@ export default function Stopwatch() {
         <div className='font'>{("0" + Math.floor((time / 3600000) % 60)).slice(-2)}:</div>
         <div className='font'>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</div>
         <div className='font'>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</div>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        <div className='font-small'>{("0" + ((time / 10) % 100)).slice(-2)}</div>
       </div>
 
       <div className='justify'>
         {isRunning ?
-          <button className='button-main button-black' onClick={() => setIsRunning(false)}>Stop</button>:
+          <button className='button-main button-blue' onClick={() => setIsRunning(false)}>Stop</button>:
           <button className='button-main' onClick={() => setIsRunning(true)}>Start</button>
         }
         <button className='button-main button-black' onClick={() => setTime(0)}>Reset</button> 
