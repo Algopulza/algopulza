5fe8af30c35978faa2665db611f801bd375b6386
김혜지 <hengzizng@naver.com>
2022-05-11 02:51:18 +0900

    Feat: ProblemMark Entity 생성

---
+++ backend/src/main/java/com/algopulza/backend/db/entity/ProblemMark.java
@@ -0,0 +1,29 @@
+package com.algopulza.backend.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "problem_mark")
+public class ProblemMark extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id")
+    private Problem problem;
+
+    @Column
+    private int markType;
+
+}
