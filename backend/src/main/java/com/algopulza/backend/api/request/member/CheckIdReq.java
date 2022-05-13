f294c5256d03ce5c23bdaa51cf55cefd5cf35b16
김혜지 <hengzizng@naver.com>
2022-05-13 13:36:07 +0900

    Test: id중복검사 테스트

---
+++ backend/src/main/java/com/algopulza/backend/api/request/member/CheckIdReq.java
@@ -0,0 +1,13 @@
+package com.algopulza.backend.api.request.member;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckIdReq {
+
+    private String id;
+
+}
