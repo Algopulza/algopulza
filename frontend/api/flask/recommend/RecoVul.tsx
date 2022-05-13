de164146d0c9e14a93757744c85250526396abbf
runkey <csi9509@naver.com>
2022-05-13 19:52:49 +0900

    Feat : 취약 태그 추천 api 변수타입 수정

--- frontend/api/flask/recommend/RecoVul.tsx
+++ frontend/api/flask/recommend/RecoVul.tsx
@@ -2,6 +2,6 @@ import { flaskapi } from "../Flaskapi";
 
 // 취약태그 추천
 // 푼 태그 중 푼 문제 가장 적은 태그에서 문제 추천
-export const getRecoVul = async (accessToken:string, memberId:string) => {
-  return await flaskapi(accessToken).get(`/recomm/vulnerability/${memberId}`).then().catch();
+export const getRecoVul = async (accessToken:string, bojId:string) => {
+  return await flaskapi(accessToken).get(`/recomm/vulnerability/${bojId}`).then().catch();
 };
