7c55b90ddba06f74e9f1972b2f5fd16a841278c9
runkey <csi9509@naver.com>
2022-05-13 19:57:07 +0900

    Feat: 기록 페이지 디자인

--- frontend/components/mypage/record/Index.tsx
+++ frontend/components/mypage/record/Index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import Favorite from './Favorite';
-import Solved from './Solved';
+import Favorite from "./Favorite";
+import Solved from "./Solved";
 
 const Container = styled.div`
   display: grid;
