f334908e6335f597bbfec73d107943075db60800
runkey <csi9509@naver.com>
2022-05-13 19:53:55 +0900

    Feat: 총 코드 길이 디자인 및 api 추가

--- frontend/components/mypage/analysis/Code.tsx
+++ frontend/components/mypage/analysis/Code.tsx
@@ -3,20 +3,25 @@ import styled from "styled-components";
 import AnalyTitle from "../../common/AnalyTitle";
 
 const Container = styled.div`
-  width: 90%;
-  height: 90%;
+  width: 30vw;
+  height: 33vh;
   border-radius: 10px;
   box-shadow: 0px 4px 4px 0 rgba(0, 0, 0, 0.25);
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 1rem;
+  font-size: 1.5rem;
 `;
-
-const Code = () => {
+type CODE = {
+  length:number
+}
+const Code = ({length}:CODE) => {
   return (
     <Container>
       <AnalyTitle>총 코드 길이</AnalyTitle>
+      {length}B
     </Container>
   );
 };
