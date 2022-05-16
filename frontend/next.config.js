7358d67db10f4871c749dc83500149288211e94e
2dend <2dend0713@gmail.com>
2022-05-17 02:19:23 +0900

    Fix: 새로고침 버그, config 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "/",
-  },
-};
+  // images: {
+  //   loader: "akamai",
+  //   path: "/",
+  // }
+}
 
-module.exports = nextConfig;
+module.exports = {
+  nextConfig,
+  images: {
+    loader: 'imgix',
+    path: 'https://k6a408.p.ssafy.io/',
+  },
+  trailingSlash: true,
+}
