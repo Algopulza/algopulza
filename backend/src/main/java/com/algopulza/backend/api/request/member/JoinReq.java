441fb84820a4d809770dd41b5c65f7f781a6dd89
minjunglee <3sally@naver.com>
2022-05-16 17:45:56 +0900

    S06P31A408-236 Fix : 회원가입 로직 수정 - 이미지 인식 제거

--- backend/src/main/java/com/algopulza/backend/api/request/member/JoinReq.java
+++ backend/src/main/java/com/algopulza/backend/api/request/member/JoinReq.java
@@ -3,7 +3,6 @@ package com.algopulza.backend.api.request.member;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
 
 @ApiModel("JoinReq")
 @Data
@@ -14,8 +13,8 @@ public class JoinReq {
     @ApiModelProperty(value = "알고풀자 비밀번호", required = true)
     private String password;
 
-    @ApiModelProperty(value = "백준 정보 캡쳐 이미지", required = true)
-    private MultipartFile capturedImage;
+    @ApiModelProperty(value = "백준 아이디", required = true)
+    private String bojId;
 
     @ApiModelProperty(value = "백준 문제 풀이 내역(solved)", required = false)
     private String solvedProblems;
