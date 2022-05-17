bcf37893110fe9489f2ed8eb0b6a73742433685e
runkey <csi9509@naver.com>
2022-05-17 20:48:31 +0900

    Style: 카테고리 버튼 디자인 수정

--- frontend/components/mypage/analysis/Index.tsx
+++ frontend/components/mypage/analysis/Index.tsx
@@ -21,12 +21,12 @@ const Select = styled.div`
 `
 
 const CategoryButton = styled.button<{bg:string, hbg:string, hc:string}>`
-  height: 2rem;
-  width: 8rem;
+  height: 7vh;
+  width: 10vw;
   font-size: 0.8rem;
   border: none;
   border-radius: 15px;
-  box-shadow: 0px 4px 10px 6px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   background-color: ${(props) => (props.bg ? props.bg : "")};
   color: ${(props) => (props.color ? props.color : "")};
   font-weight: 700;
@@ -77,8 +77,8 @@ const Index = ({accessToken, memberId, bojId}:User) => {
     <Container>
       <Select>
         <CategoryButton onClick={showWeakness} color={weakness?"white":"white"} bg={weakness?"#282828":"#A1A1A1"} hbg={weakness?"#404040":"#8F8F8F"} hc={weakness?"white":"white"}>취약점</CategoryButton>
-        <CategoryButton onClick={showLanguage} color={language?"white":"white"} bg={language?"#282828":"#A1A1A1"} hbg={language?"#404040":"#8F8F8F"} hc={language?"white":"white"}>사용언어 비율</CategoryButton>
         <CategoryButton onClick={showTag} color={tag?"white":"white"} bg={tag?"#282828":"#A1A1A1"} hbg={tag?"#404040":"#8F8F8F"} hc={tag?"white":"white"}>태그 별 해결 문제 수</CategoryButton>
+        <CategoryButton onClick={showLanguage} color={language?"white":"white"} bg={language?"#282828":"#A1A1A1"} hbg={language?"#404040":"#8F8F8F"} hc={language?"white":"white"}>사용언어 비율</CategoryButton>
         <CategoryButton onClick={showMonth} color={month?"white":"white"} bg={month?"#282828":"#A1A1A1"} hbg={month?"#404040":"#8F8F8F"} hc={month?"white":"white"}>월 별 문제 풀이 갯수</CategoryButton>
       </Select>
 
