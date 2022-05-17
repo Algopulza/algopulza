492c8036af218139ee2cf9ba913bdb1697bb1f61
runkey <csi9509@naver.com>
2022-05-17 16:31:37 +0900

    Feat: 스피너 바깥으로 옮김

--- frontend/components/recommendation/Subject.tsx
+++ frontend/components/recommendation/Subject.tsx
@@ -2,9 +2,6 @@ import { SubjectAttr, ProblemAttr } from '../../util/dto'
 import SubjectTitle from './SubjectTitle'
 import Card from '../common/card/Card'
 import styled from 'styled-components'
-import Image from 'next/image';
-import brand_logo from '../../public/common/brand_logo.png'
-import { useEffect, useState } from 'react';
 
 const Container = styled.section`
 margin-bottom: 80px;
@@ -15,47 +12,16 @@ const Cards = styled.div`
   justify-content: space-between;
 `
 
-const CircularProgressContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 5rem;
-  height: 5rem;
-  @keyframes logoSpin {
-        0% { transform: rotate(0deg); }
-        100% { transform: rotate(360deg); }
-    }
-    #logo {
-        animation: logoSpin 2s linear infinite;
-    }
-`
-
 type SubjectProps = {
   subjectAttr: SubjectAttr
 }
 
 export default function Subject({ subjectAttr }: SubjectProps) {
   const lists = subjectAttr.list
-  const [ alert, setAlert ] = useState(true)
-  console.log(subjectAttr)
-
-  useEffect(() =>{
-    setTimeout(() => {setAlert(false)},2000)
-  },[])
 
   return (
     <Container>
-      
-      { !lists ?
-      <>
-        {alert?
-        <CircularProgressContainer>
-          <Image id="logo" src={brand_logo} alt="" />
-        </CircularProgressContainer>
-        :null}
-      </>
-      : null }
-
-      {lists ? <SubjectTitle>{subjectAttr}</SubjectTitle> : null}
+     {lists ? <SubjectTitle>{subjectAttr}</SubjectTitle> : null}
       
       <Cards>
         {lists && lists.map((list: ProblemAttr) =>
