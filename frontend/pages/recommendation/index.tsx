2173a50e93cd1ed286806f744f21e49ac4b097fa
runkey <csi9509@naver.com>
2022-05-17 16:32:18 +0900

    Feat: 스피너 바깥으로 뺌

--- frontend/pages/recommendation/index.tsx
+++ frontend/pages/recommendation/index.tsx
@@ -10,12 +10,27 @@ import { getRecoVul } from '../../api/flask/recommend/RecoVul'
 import { getRecoTag } from '../../api/flask/recommend/RecoTag'
 import { getSolvedTear } from '../../api/flask/recommend/RecoSolvedTear'
 import { getRecoTear } from '../../api/flask/recommend/RecoTear'
+import { CircularProgress } from "@mui/material";
 
 const Container = styled.section`
-  padding: 0vw 5vw;
+  padding: 0vh 5vw;
+`
+
+const CircularProgressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 40vh;
+  `
+
+const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 export default function Recommendation() {
+  const [alert, setAlert ] = useState(true)
   const [vulData, setVulData] = useState()
   const [tagData, setTagData] = useState()
   const [solvedData, setSolvedData] = useState()
@@ -60,7 +75,12 @@ export default function Recommendation() {
     RecommendTag()
     RecommendSolved()
     RecommendTear()
+    console.log(1)
   }, [])
+
+  useEffect(() =>{
+    setTimeout(() => {setAlert(false)},2000)
+  },[])
 
   const subjects = [
     {title: '최근에 자주 해결한 태그에 속하는 문제를 추천해 드려요!', englishTitle: '', list: tagData},
@@ -71,10 +91,21 @@ export default function Recommendation() {
 
   return (
     <>
+      {alert?
+        <CircularProgressContainer>
+          <SubContainer>
+            <CircularProgress sx={{ display: 'flex', justifyContent: 'center', marginBottom:"1rem" , color: '#282828' }}/>
+            <div>사용자에게 맞는 추천 문제를 뽑아오고 있어요!</div>
+          </SubContainer>
+        </CircularProgressContainer>
+        :
+        <>
       <Gift />
       <Container>
         {subjects.map((subject) => <Subject key={subject.title} subjectAttr={subject} />)}
       </Container>
+      </>
+      }
     </>
   )
 }
