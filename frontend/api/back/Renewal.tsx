cdf0030e4706715cbcabd6546432bb59500f0e20
runkey <csi9509@naver.com>
2022-05-19 01:51:42 +0900

    Feat: 리뉴얼 API 삭제

--- frontend/api/back/Renewal.tsx
+++
@@ -1,5 +0,0 @@
-import { backapi } from "./BackApi";
-
-export const getRenewal = async (accessToken:string, bojId:string) => {
-  return await backapi(accessToken).post("/members/renewal", bojId).then().catch();
-};
