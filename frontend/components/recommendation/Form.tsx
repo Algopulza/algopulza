3a88ddc0cfac4dbde9d745b1466d1ed974faad77
2dend <2dend0713@gmail.com>
2022-05-19 10:14:28 +0900

    Fix: build 오류 수정

--- frontend/components/recommendation/Form.tsx
+++ frontend/components/recommendation/Form.tsx
@@ -59,7 +59,7 @@ export default function Form() {
 
       <InputContainer>
         <Board>
-          <FormSolved />
+          {/* <FormSolved /> */}
           <FormTried />
         </Board>
       </InputContainer>
