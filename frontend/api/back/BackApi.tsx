750feb3388e4618939b45c33d7388252b9290c71
2dend <2dend0713@gmail.com>
2022-05-27 15:48:00 +0900

    Refactor: "api base url 수정"

--- frontend/api/back/BackApi.tsx
+++ frontend/api/back/BackApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const backapi = (accessToken: string) =>
   axios.create({
-    baseURL: "https://k6a408.p.ssafy.io/api/v1/",
+    baseURL: "https://algopulza.day/api/v1/",
     headers: {
       "Content-Type": `application/json`,
       Authorization: `Bearer ${accessToken}`
