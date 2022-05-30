24b19b3bb9eec8636be965cb2e983f737068dbd2
hengzizng <hengzizng@naver.com>
2022-05-31 01:51:20 +0900

    Fix: Flask API 주소 변경

--- frontend/api/flask/Flaskapi.tsx
+++ frontend/api/flask/Flaskapi.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const flaskapi = (accessToken: string) =>
   axios.create({
-    baseURL: "https://algopulza.day/api/",
+    baseURL: "https://algopulza.day/flask-api/",
     headers: {
       "Content-Type": `application/json`,
       Authorization: `Bearer ${accessToken}`
